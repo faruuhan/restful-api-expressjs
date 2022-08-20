c348e25f02a1e98309ded362cbc45afdd3cb7446
farhan <farhnn.r@gmail.com>
2022-08-20 07:20:39 +0700

    endpoin deleteUser

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -50,8 +50,18 @@ const createUser = async (req, res) => {
 
 const getAllUser = async (req, res) => {
   const data = await usersModel.findAll();
-  const response = { status: 200, data: data };
+  const datafilter = await data.map(({ id, fullName, email, userImg }) => {
+    return { id, fullName, email, userImg };
+  });
+  const response = { status: 200, data: datafilter };
   return res.status(200).json(response);
 };
 
-module.exports = { createUser, getAllUser };
+const deleteUser = async (req, res) => {
+  await usersModel.destroy({ where: { id: req.params.id } });
+
+  const response = { status: 201, data: "data deleted" };
+  return res.status(201).json(response);
+};
+
+module.exports = { createUser, getAllUser, deleteUser };
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,9 @@
 var express = require("express");
 var router = express.Router();
-var { createUser, getAllUser } = require("../controllers/users.controller");
+var { createUser, getAllUser, deleteUser } = require("../controllers/users.controller");
 
 router.post("/", createUser);
 router.get("/", getAllUser);
+router.delete("/:id", deleteUser);
 
 module.exports = router;
